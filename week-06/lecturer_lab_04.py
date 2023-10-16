5e50b20fbe073a429d6aee11f26869afe7b99cee
CheeseLad <cheesescreativeserver@gmail.com>
2023-10-16 14:27:12 +0100

    q6 lecture

--- week-06/lecturer_lab_04.py
+++ week-06/lecturer_lab_04.py
@@ -62,3 +62,23 @@ for char in input_str:
         empty_q.enqueue(char)
     else:
         print(empty_q.dequeue())
+
+# Q6
+
+input_formula = "1432^*+147--+"
+operator_lst = ["^", "*", "+", "-", "/"]
+
+empty_stack = Stack()
+for char in input_formula:
+    if char not in operator_lst:
+        empty_stack.push(float(char))
+    else:
+        b = empty_stack.pop()
+        a = empty_stack.pop()
+
+        c = {"+": a + b, "-": a - b, "*": a * b, "/": a / b, "^": a ** b}[char]
+        empty_stack.push(c)
+    print(empty_stack.items)
+
+# Time complexity: the "in" operator checks a small amount in list (not worth adding to time complexity)
+
